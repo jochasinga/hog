d42b034ce59cedba509c4a05fa10dfded9fd34c6
Panisuan Chasinga <jo.chasinga@gmail.com>
2015-11-13 19:30:17 -0500

    Add comments for godoc

--- hog.go
+++ hog.go
@@ -1,3 +1,72 @@
+/*
+Package hog is a wrapper around Go `crypto` package for quick hashing of passwords
+with salts appended.
+
+Currently only supporting `md5`, `sha1`, and `sha256` packages for both the
+salt and the combination.
+
+Examples
+
+Create an instance of `Combination` and supply the hash function type
+constant (MD5, SHA1, SHA256) and a password string. Without providing
+a salt, it will be created with SHA1 with `Salt.Size = 16` and append to
+the password before being hashed.
+
+	package main
+
+		import (
+			"fmt"
+			"github.com/jochasinga/hog"
+		)
+
+		func main() {
+			pw := hog.Combination{
+				Func: hog.SHA1,
+				Secret: "supersecretpassword",
+			}
+
+			hogged := pw.New()           // return a []byte of hashed combination
+			fmt.Println(string(hogged))  // 0a12a5f5046bdea95f1ba8f8a570d7361922c0a9
+		}
+
+A `Salt` can be created separately before using in the `Combination`
+
+	func main() {
+
+		password := "superStrongPassword321"
+
+		salt := hog.Salt{
+			Func: hog.MD5,
+			Secret: password,
+			Size: 16,
+
+		}
+
+		bsalt := salt.New()
+
+		pw := hog.Combination{
+			Func: hog.SHA256,
+			Secret: password,
+			Salt: bsalt
+		}
+
+		hogged := pw.New()
+	}
+
+Or use helper functions to create hash
+
+	func main() {
+
+		// Same variables as previous
+
+		bsalt := hog.CreateHash(salt)
+		hogged := hog.CreateHash(pw)
+
+		// or just create default combination from password string
+		fin := hog.CreateHashFromString(password)
+
+	}
+*/
 package hog
 
 import (
@@ -118,32 +187,39 @@ func (c Combination) New() []byte {
 	return pwhash.Sum(nil)
 }
 
-/***** Helper Functions *****/
+/* Helper Functions */
+
+// CreateHash will create a hashed `[]byte` from a `Salt` or `Combination`
+// instance.
 func CreateHash(h Hog) []byte {
 	return h.New()
 }
 
+// CreateHashFromString will create a hashed combination based on a provided
+// password string.
 func CreateHashFromString(secret string) []byte {
 
 	salt := generateSalt(secret, 16)
 
 	c := Combination{
-		Func: SHA1,
+		Func:   SHA1,
 		Secret: secret,
-		Salt: salt,
+		Salt:   salt,
 	}
 
 	return c.New()
 }
 
+// Match two `Hog` instance i.e. password matching
 func Match(h1, h2 Hog) bool {
 	match := bytes.Equal(h1.New(), h2.New())
 	return match
 }
 
+// Private function
 func generateSalt(secret string, saltSize uint) []byte {
 	sc := []byte(secret)
-	buf := make([]byte, saltSize, saltSize + sha1.Size)
+	buf := make([]byte, saltSize, saltSize+sha1.Size)
 	_, err := io.ReadFull(rand.Reader, buf)
 
 	if err != nil {
